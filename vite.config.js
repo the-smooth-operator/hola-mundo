05af8ece833010b6784f09fa9311d8b0fd7c090d
Alberto del Barrio <alberto.delbarrio.albelda@gmail.com>
2025-05-16 23:04:45 +0200

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://the-smooth-operator.github.io/hola-mundo/',
   plugins: [react()],
+  base: '/hola-mundo/',
 })
