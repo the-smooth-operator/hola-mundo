45794fa5f58ad7894b069b632fdd3371c1f004ec
Alberto del Barrio <alberto.delbarrio.albelda@gmail.com>
2025-05-16 23:05:09 +0200

    disable sourcemap

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/hola-mundo/',
+  build: {
+    sourcemap: false, // Disable source maps to avoid source map errors
+  },
 })
