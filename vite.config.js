5aafc406f0337695c20f30d7a93ccc1f1ed39ee6
Alberto del Barrio <alberto.delbarrio.albelda@gmail.com>
2025-05-16 23:28:27 +0200

    add base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: '/hola-mundo/',
+  base: '/hola-mundo/',
   build: {
     sourcemap: false, // Disable source maps to avoid the "URL constructor" error
   },
