d802a5b7b35d98368c0e2e96d6cafa507b663c25
Alberto del Barrio <alberto.delbarrio.albelda@gmail.com>
2025-05-16 22:58:29 +0200

    hardcode base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE || '/',
+  base: 'https://the-smooth-operator.github.io/hola-mundo/',
   plugins: [react()],
 })
